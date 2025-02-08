71f20ef447887d9b63275f7689a402cf44076cc1
Daniel Ilievski <dilievski54@gmail.com>
2025-02-08 19:18:51 +0100

    Improve event details modal UI part 2

--- src/components/EventDetailsModal.vue
+++ src/components/EventDetailsModal.vue
@@ -7,7 +7,7 @@
       <div class="bg-white w-[90%] max-w-3xl p-6 rounded-lg shadow-lg relative transform transition-all">
         <!-- Header -->
         <div class="flex items-center justify-between pb-3">
-          <h2 v-if="props.selectedEvent?.summary" class="text-xl font-bold text-gray-800">
+          <h2 v-if="props.selectedEvent?.summary" class="text-2xl font-bold text-gray-800">
             {{ props.selectedEvent.summary }}
           </h2>
           <button
@@ -21,7 +21,7 @@
         <!-- Event Info -->
         <div class="mt-4 space-y-3 text-gray-700">
 
-          <div class="rounded-md shadow-md p-5 text-md">
+          <div class="rounded-md shadow-[0_2px_8px_rgba(0,0,0,0.4)] px-5 py-2.5 text-md">
             <p >
               <i class="bi bi-geo-alt-fill mr-2 text-red-500"></i>{{
                 props.selectedEvent?.location || ' No location provided'
@@ -40,8 +40,8 @@
               }}
             </p>
 
-            <p>
-              <i class="bi bi-repeat mr-2 text-purple-500"></i> {{
+            <p class="text-gray-400 mt-3">
+             {{
                 getFrequency(props.selectedEvent?.freq,
                     props.selectedEvent?.byDay,
                     props.selectedEvent?.interval,
@@ -92,33 +92,35 @@ const getSanitizedDescription = (description) => {
 
 const getFrequency = (freq, byDay, interval, wkst, until, count) => {
   console.log("inside get freq")
-  let text = 'This event occurs ';
+  let text = 'This event ';
 
   switch (freq) {
     case 'DAILY':
-      text += interval ? `every ${interval} days` : 'daily';
+      text += interval ? `occurs every ${interval} days` : 'daily';
       break;
 
     case 'WEEKLY':
       // handled in default because of edge cases
-      break;
 
     case 'YEARLY':
-      text += interval ? `every ${interval} years` : 'annually';
+      text += interval ? `occurs every ${interval} years` : 'annually';
       break;
 
     default:
+
       if (byDay) {
         if(typeof byDay === 'string')
           byDay = Array.of(byDay)
 
         if(byDay.length >= 1) {
-          text += `on ${byDay.map(getFullDay).join(', ')}`;
+          if(wkst) {
+            text += wkst > 1 ? `occurs every ${wkst} weeks` : ' every week';
+          }
+          text += ` on ${byDay.map(getFullDay).join(', ')}`;
+          break
         }
-
-        text += wkst > 1 ? ` every ${wkst} weeks` : ' every week';
-        break
       }
+
       text += 'takes place one time only';
       break
   }
