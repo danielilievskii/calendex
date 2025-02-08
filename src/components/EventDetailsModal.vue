c726657230ad3983153ab209624d7724aebe5c2d
Daniel Ilievski <dilievski54@gmail.com>
2025-02-08 02:50:29 +0100

    Improve event details modal UI

---
+++ src/components/EventDetailsModal.vue
@@ -0,0 +1,147 @@
+<template>
+  <!-- Modal Background -->
+  <Transition name="fade">
+    <div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+      <!-- Modal Box -->
+
+      <div class="bg-white w-[90%] max-w-3xl p-6 rounded-lg shadow-lg relative transform transition-all">
+        <!-- Header -->
+        <div class="flex items-center justify-between pb-3">
+          <h2 v-if="props.selectedEvent?.summary" class="text-xl font-bold text-gray-800">
+            {{ props.selectedEvent.summary }}
+          </h2>
+          <button
+              class="text-gray-400 hover:text-gray-700"
+              @click="toggleModal()"
+          >
+            <i class="fa-solid fa-xmark text-2xl"></i>
+          </button>
+        </div>
+
+        <!-- Event Info -->
+        <div class="mt-4 space-y-3 text-gray-700">
+
+          <div class="rounded-md shadow-md p-5 text-md">
+            <p >
+              <i class="bi bi-geo-alt-fill mr-2 text-red-500"></i>{{
+                props.selectedEvent?.location || ' No location provided'
+              }}
+            </p>
+
+            <p >
+              <i
+                  :class="'bi bi-clock text-blue-500'"
+                  class="mr-2"
+              ></i>
+              {{
+                props.selectedEvent?.startDate === props.selectedEvent?.endDate
+                    ? formatDateToString(props.selectedEvent?.startDateTime || '') + ' ' + (props.selectedEvent?.formattedDuration || '')
+                    : formatDateTimeToString(props.selectedEvent?.startDateTime || '') + ' - ' + formatDateTimeToString(props.selectedEvent?.endDateTime || '')
+              }}
+            </p>
+
+            <p>
+              <i class="bi bi-repeat mr-2 text-purple-500"></i> {{
+                getFrequency(props.selectedEvent?.freq,
+                    props.selectedEvent?.byDay,
+                    props.selectedEvent?.interval,
+                    props.selectedEvent?.wkst,
+                    props.selectedEvent?.until,
+                    props.selectedEvent?.count)
+              }}
+            </p>
+
+          </div>
+
+
+          <p class="text-md" v-html="getSanitizedDescription(props.selectedEvent?.description)"></p>
+        </div>
+
+        <!-- Close Button -->
+        <div class="flex justify-center mt-4">
+          <button
+              class="bg-[#52b788] text-white px-4 py-2 rounded-md shadow-md hover:bg-[#409f69] transition"
+              @click="toggleModal()"
+          >
+            Close
+          </button>
+        </div>
+      </div>
+
+    </div>
+  </Transition>
+</template>
+
+<script lang="ts" setup>
+import DOMPurify from 'dompurify';
+
+import {defineProps} from 'vue';
+import {formatDateToISO, formatDateToString, formatDateTimeToString, getFullDay, formatDateToStringShort} from "../utils/dateUtils.js";
+import {} from "../utils/dateUtils.js";
+
+// Define props
+const props = defineProps<{
+  showModal: boolean;
+  toggleModal: Function;
+  selectedEvent?: any;
+}>();
+
+const getSanitizedDescription = (description) => {
+  return description ? DOMPurify.sanitize(description) : 'No description provided.';
+};
+
+const getFrequency = (freq, byDay, interval, wkst, until, count) => {
+  console.log("inside get freq")
+  let text = 'This event occurs ';
+
+  switch (freq) {
+    case 'DAILY':
+      text += interval ? `every ${interval} days` : 'daily';
+      break;
+
+    case 'WEEKLY':
+      // handled in default because of edge cases
+      break;
+
+    case 'YEARLY':
+      text += interval ? `every ${interval} years` : 'annually';
+      break;
+
+    default:
+      if (byDay) {
+        if(typeof byDay === 'string')
+          byDay = Array.of(byDay)
+
+        if(byDay.length >= 1) {
+          text += `on ${byDay.map(getFullDay).join(', ')}`;
+        }
+
+        text += wkst > 1 ? ` every ${wkst} weeks` : ' every week';
+        break
+      }
+      text += 'takes place one time only';
+      break
+  }
+
+  if (until) {
+    text += `, ending on ${formatDateToStringShort(until)}.`;
+  } else if (count) {
+    text += ` for a total of ${count} occurrences.`;
+  }
+
+  return text;
+};
+</script>
+
+
+<style>
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.1s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
